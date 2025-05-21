769dc039b1a0a2ec216c5b43fe11069c44b7c582
anjalirana32 <anjalirana3215@gmail.com>
2025-05-21 19:42:00 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 1443222
+let accountEmail = "anjalirana3215@gmail.com"
+var accountPassword = "23233"
+accountCity = "Himachal"
+let accountState;
+
+
+// accountId = 2  // not allowed 
+
+
+
+accountEmail = "fhdh.com"
+accountPassword ="2324424"
+accountCity ="Chandigarh"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+beacuse of issue in block scope and functional scope
+*/
+
+console.table[accountId, accountEmail, accountPassword, accountCity, accountState]
+
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // trest all JS code as newer version
+
+// alert("hello")  // we are using nodejs , not browser
+
+console.log(9 + 9); 
+
+console.log("Anjali")
+
+let name = "anjali"
+let age = 45
+let isLoggedIn = false
+
+
